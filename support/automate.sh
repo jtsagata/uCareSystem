93c25feeb52b861055e2ff67fc0c7a172d307767
Giannis Tsagkatakis <jtsagata@gmail.com>
2020-03-03 20:53:14 +0200

    Revert to unsigned release

--- support/automate.sh
+++ support/automate.sh
@@ -4,22 +4,6 @@ set -ex
 top_dir="$(git rev-parse --show-toplevel)"
 cd "$top_dir"
 
-# PGP
-declare -r custom_gpg_home="${top_dir}/.ci"
-declare -r secring_auto="${custom_gpg_home}/secring.auto"
-declare -r pubring_auto="${custom_gpg_home}/pubring.auto"
-
-echo
-echo "Decrypting secret gpg keyring.."
-# $super_secret_password is taken from the script's env.
-# https://blogs.itemis.com/en/secure-your-travis-ci-releases-part-2-signature-with-openpgp
-gpg --batch --passphrase $PGP_SECRET "${secring_auto}".gpg
-if [ $retval -ne 0 ]; then
-    echo "Failed to decrypt secret gpg keyring."
-    exit 1
-fi
-echo Success!
-
 # install dependencies
 sudo apt-get update
 sudo apt-get install -y devscripts equivs
